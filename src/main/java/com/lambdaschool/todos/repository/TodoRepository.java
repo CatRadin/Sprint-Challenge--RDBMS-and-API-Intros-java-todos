238717022e2971b908fe63486e7ce10477b3a980
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-03-19 12:38:39 -0500

    Added todod

--- src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
+++ src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
@@ -1,2 +1,9 @@
-package com.lambdaschool.todos.repository;public interface TodoRepository {
+package com.lambdaschool.todos.repository;
+
+import com.lambdaschool.todos.models.Todos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository<Todos, Long>
+{
+
 }
